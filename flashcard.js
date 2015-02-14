be5b11f9c1615ccd497e612627076cf15be6b1c0
Jeff Everhart <Jeffeverhart383@gmail.com>
2015-02-14 18:16:16 -0500

    added support for card change radios

--- flashcard.js
+++ flashcard.js
@@ -1,53 +1,149 @@
 //Flashcard JS Game
 //By Jeff Everhart
 
-//load array of cards in JSON
 
-var cards = [
+
+$("document").ready(function(){
+
+//load array of cards in JSON
+var cards3 = [
+
+{
+	
+	"word":"whack",
+	"definition":"a mole",
+	"difficulty": 0
+}, 
+{
+	"word":"something",
+	"definition":"bad",
+	"difficulty": 0
+},
+{
+	"word":"what",
+	"definition":"happends",
+	"difficulty": 0
+},
+{
+	"word":"TBONE",
+	"definition":"Walker",
+	"difficulty": 0
+},
+{
+	"word":"BB",
+	"definition":"King",
+	"difficulty": 0
+},
+{
+	"word":"Eric",
+	"definition":"Clapton",
+	"difficulty": 0
+}];
+
+
+var cards2 = [
+
+{
+	
+	"word":"cards2",
+	"definition":"bcards2",
+	"difficulty": 0
+}, 
+{
+	"word":"cards2",
+	"definition":"World",
+	"difficulty": 0
+},
+{
+	"word":"John",
+	"definition":"Coltrane",
+	"difficulty": 0
+},
+{
+	"word":"TBONE",
+	"definition":"Walker",
+	"difficulty": 0
+},
+{
+	"word":"BB",
+	"definition":"King",
+	"difficulty": 0
+},
+{
+	"word":"Eric",
+	"definition":"Clapton",
+	"difficulty": 0
+}];
+
+var cards1 = [
 
 {
 	
 	"word":"foo",
 	"definition":"bar",
-	"difficulty": 3
+	"difficulty": 0
 }, 
 {
 	"word":"Hello",
 	"definition":"World",
-	"difficulty": 2
+	"difficulty": 0
 },
 {
 	"word":"John",
 	"definition":"Coltrane",
-	"difficulty": 1
+	"difficulty": 0
 },
 {
 	"word":"TBONE",
 	"definition":"Walker",
-	"difficulty": 2
+	"difficulty": 0
 },
 {
 	"word":"BB",
 	"definition":"King",
-	"difficulty": 3
+	"difficulty": 0
 },
 {
 	"word":"Eric",
 	"definition":"Clapton",
-	"difficulty": 1
+	"difficulty": 0
 }];
+	
 
+$("input:radio[name=card-select]").click(function(){
+
+var cards; 
 var counter = 0;
 var firstTime = true;
 
+
+
+if ($(this).val() === "cards1"){
+	cards = cards1;
+
+}
+
+if ($(this).val() === "cards2"){
+	cards = cards2;
+
+}
+
+if ($(this).val() === "cards3"){
+	cards = cards3;
+
+}	
+
+
+//clear previous progress grid
+$('.progress-grid').empty();
 
 //Display first card from array
 
 $(".vocab-term").text(cards[counter].word);
 $(".vocab-def").text(cards[counter].definition);
 
-for( var i = 0; i <= cards.length; i ++)
-	$(".progress-grid").append('<span class="box default"></span>');
+for( var i = 0; i < cards.length; i ++)
+	$(".progress-grid").append('<span class="box default">&#10004;</span>');
 
 
 function changeCard(diffLevel){
@@ -57,7 +153,7 @@ function changeCard(diffLevel){
 			$(".box").eq(counter).addClass("easy");
 		}
 
-	counter = counter + 1;
+	counter += 1;
 	if (counter >= cards.length){
 			counter = 0;
 			firstTime = false;
@@ -88,3 +184,8 @@ $(".easy").click(function(){
 	changeCard(3);
 
 });
+
+});//End on radio change card load
+
+
+});//End document ready function
